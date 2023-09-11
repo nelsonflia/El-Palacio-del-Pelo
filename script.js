c7f799801b16eb21f28218e473ae0086708a95ab
nelsonflia <fam16002@byui.edu>
2023-09-11 12:10:01 -0500

    Terminar pagina de inicio
    Faltan los toques finales pero los elementos ya están agregados

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/* ----------------------- Mobile Header ------------------ */
+const bar = document.getElementById('bar');
+const cerrar = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
+}
+
+if (cerrar) {
+  cerrar.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
+}
+
+/* ----------------------- Search Bar ------------------ */
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
 let searchBar = document.querySelector(".searchBar");
